26cee2ba2f3eb5ecb0266d5fbf8815ca7e269dd7
Daniel Suarez <dansuarmar@gmail.com>
2023-11-27 14:41:35 -0500

    List Component with Values on HTML

---
+++ introductiontoreact/pages/index.js
@@ -0,0 +1,18 @@
+export default function Home() {
+  function ListItems() {
+    return (
+      <>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </>
+    );
+  }
+
+  return (
+    <ul>
+      <ListItems></ListItems>
+    </ul>
+  )
+}
