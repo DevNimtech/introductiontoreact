7abdef7984013540a48e49b478135116ee30eedf
Daniel Suarez <dansuarmar@gmail.com>
2023-12-11 13:05:59 -0500

    Additional way to show variable.
    2 lines show different ways to access variables.

--- reactcompleteguide/01-starting-project/src/Header.jsx
+++ reactcompleteguide/01-starting-project/src/Header.jsx
@@ -1,16 +1,18 @@
-function Header() {
-    const options = ['Fundamental','Crucial','Core'];
+const options = ['Fundamental','Crucial','Core'];
     function getRangomInt(max){
         return Math.floor(Math.random() * (max + 1));
     }
+
+function Header() {
+    const firstWord = options[getRangomInt(3)];
 
     return(
         <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>React Essentials - YES</h1>
         <p>
-          {options[getRangomInt(3)]} React concepts you will need for almost any app you are
-          going to build!
+          {options[getRangomInt(3)]} React concepts you will need for almost any app you are going to build!<br/>
+          {firstWord} React concepts you will need for almost any app you are going to build!
         </p>
       </header>
     );
