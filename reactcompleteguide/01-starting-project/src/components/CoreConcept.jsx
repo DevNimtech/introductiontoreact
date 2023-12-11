d83591a36081f1f4f18066f6f84da80e6ba1daf4
Daniel Suarez <dansuarmar@gmail.com>
2023-12-11 14:15:28 -0500

    Adding additional prop concepts to CoreConcept.jsx Grouping and Default

--- reactcompleteguide/01-starting-project/src/components/CoreConcept.jsx
+++ reactcompleteguide/01-starting-project/src/components/CoreConcept.jsx
@@ -1,6 +1,17 @@
 
 //Desctructuring
-export function CoreConcept({ title, description, image }) {
+// export function CoreConcept({ title, description, image }) {
+//     return(
+//         <li>
+//             <img src={image} alt={title} />
+//             <h3>{title}</h3>
+//             <p>{description}</p>
+//         </li>
+//     );
+// }
+
+//Default Values
+export function CoreConcept({ title, description = "Optional Desctiption.", image }) {
     return(
         <li>
             <img src={image} alt={title} />
@@ -9,6 +20,17 @@ export function CoreConcept({ title, description, image }) {
         </li>
     );
 }
+
+//Grouping
+// export function CoreConcept({ ...props }) {
+//     return(
+//         <li>
+//             <img src={props.image} alt={props.title} />
+//             <h3>{props.title}</h3>
+//             <p>{props.description}</p>
+//         </li>
+//     );
+// }
 
 //Without Destructuring.
 // export function CoreConcept(props) {
