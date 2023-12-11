4644fc875015d27664b2871f13bf51754c0db2b2
Daniel Suarez <dansuarmar@gmail.com>
2023-12-11 13:02:33 -0500

    Add dynamic value to the Header.
    Shows how to modify content shown on the component.

--- reactcompleteguide/01-starting-project/src/Header.jsx
+++ reactcompleteguide/01-starting-project/src/Header.jsx
@@ -1,10 +1,15 @@
 function Header() {
+    const options = ['Fundamental','Crucial','Core'];
+    function getRangomInt(max){
+        return Math.floor(Math.random() * (max + 1));
+    }
+
     return(
         <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>React Essentials - YES</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {options[getRangomInt(3)]} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
