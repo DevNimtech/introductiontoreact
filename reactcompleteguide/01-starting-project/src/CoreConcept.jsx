69a93f522948ad15a9da0778ebfaa18b1426f824
Daniel Suarez <dansuarmar@gmail.com>
2023-12-11 13:58:56 -0500

    Desctructuring of prop
    We change the receives propt do desctructure its properties.

--- reactcompleteguide/01-starting-project/src/CoreConcept.jsx
+++ reactcompleteguide/01-starting-project/src/CoreConcept.jsx
@@ -1,9 +1,22 @@
-export function CoreConcept(props) {
+
+//Desctructuring
+export function CoreConcept({ title, description, image }) {
     return(
         <li>
-            <img src={props.image} alt={props.title} />
-            <h3>{props.title}</h3>
-            <p>{props.description}</p>
+            <img src={image} alt={title} />
+            <h3>{title}</h3>
+            <p>{description}</p>
         </li>
     );
 }
+
+//Without Destructuring.
+// export function CoreConcept(props) {
+//     return(
+//         <li>
+//             <img src={props.image} alt={props.title} />
+//             <h3>{props.title}</h3>
+//             <p>{props.description}</p>
+//         </li>
+//     );
+// }
