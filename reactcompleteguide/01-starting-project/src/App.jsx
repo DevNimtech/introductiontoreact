3d9648a2e42bf696ce89d56eb98d527f1d8be572
Daniel Suarez <dansuarmar@gmail.com>
2023-12-12 21:33:43 -0500

    Added Conditional Fromatting for the Selected Button
    Conditional Formatting
    in JSX you have tu use className for CSS class.
    We use a variable sent as a prop to the component to tell which style. In the exaple we use a boolean.
    In the component call we add the prop for the selected based on if the button is selected.

--- reactcompleteguide/01-starting-project/src/App.jsx
+++ reactcompleteguide/01-starting-project/src/App.jsx
@@ -16,16 +16,16 @@ function App() {
 
   let tabContent = <p>Pleade Select a Topic</p>;
   if (selectedTopic) {
-    tabContent = 
-    <div id="tab-content">
-      <h3>{EXAMPLES[selectedTopic].title}</h3>
-      <p>{EXAMPLES[selectedTopic].description}</p>
-      <pre>
-        <code>
-          {EXAMPLES[selectedTopic].code}
-        </code>
-      </pre>
-    </div>;
+    tabContent =
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+      </div>;
   }
 
   console.log('App Component Executing.');
@@ -50,10 +50,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'}  onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'}  onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'}  onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
           {!selectedTopic && <p>Pleade Select a Topic</p>}
           {!selectedTopic ? <p>Please Select a Topic</p> :
