86923220ea9d901c327340628420fea4a3b48e44
Daniel Suarez <dansuarmar@gmail.com>
2023-12-12 21:44:47 -0500

    List rendering even shorter with the spread operator.
    Even shorter with the spread operator:
    ```
                {CORE_CONCEPTS.map((item) =>
                  <CoreConcept key={item.title} {...item}></CoreConcept>
                )}
    ```

--- reactcompleteguide/01-starting-project/src/App.jsx
+++ reactcompleteguide/01-starting-project/src/App.jsx
@@ -37,12 +37,15 @@ function App() {
         <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-            {CORE_CONCEPTS.map((item) =>
+            {/* {CORE_CONCEPTS.map((item) =>
               <CoreConcept
                 title={item.title}
                 description={item.description}
                 image={item.image}
               ></CoreConcept>
+            )} */}
+            {CORE_CONCEPTS.map((item) =>
+              <CoreConcept key={item.title} {...item}></CoreConcept>
             )}
           </ul>
         </section>
