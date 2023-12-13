bc1a0b074d0636d57bba3dfc1fb2c7580cd677c4
Daniel Suarez <dansuarmar@gmail.com>
2023-12-12 21:25:34 -0500

    Implemented Conditional Rednering
    You can implement conditional rendering with ternary expressions. Just one liners.
    The results in any JSX have to be in one parent node.
    ```
              {!selectedTopic ? <p>Please Select a Topic</p> :
                <div id="tab-content">
                  <h3>{EXAMPLES[selectedTopic].title}</h3>
                  <p>{EXAMPLES[selectedTopic].description}</p>
                  <pre>
                    <code>
                      {EXAMPLES[selectedTopic].code}
                    </code>
                  </pre>
                </div>
              }
    ```
    You can also use the lazy operator. Since the first part is false, the second one is not evaluated, is not executed.
    {!selectedTopic} && <p>Please Select a topic</p>
    It has to be 1 liner, or, you can also use variables with JSX code inside.

--- reactcompleteguide/01-starting-project/src/App.jsx
+++ reactcompleteguide/01-starting-project/src/App.jsx
@@ -7,13 +7,27 @@ import TabButton from "./components/TabButton";
 
 
 function App() {
-  const [selectedTopic, setSelectedTopic ] = useState('components');
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect(selectedButton) {
     console.log(selectedButton);
     setSelectedTopic(selectedButton);
   }
-  
+
+  let tabContent = <p>Pleade Select a Topic</p>;
+  if (selectedTopic) {
+    tabContent = 
+    <div id="tab-content">
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>
+          {EXAMPLES[selectedTopic].code}
+        </code>
+      </pre>
+    </div>;
+  }
+
   console.log('App Component Executing.');
 
   return (
@@ -41,15 +55,19 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+          {!selectedTopic && <p>Pleade Select a Topic</p>}
+          {!selectedTopic ? <p>Please Select a Topic</p> :
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>
+                  {EXAMPLES[selectedTopic].code}
+                </code>
+              </pre>
+            </div>
+          }
+          {tabContent}
         </section>
       </main>
     </div>
