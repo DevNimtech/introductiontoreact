787b9a9d0cd8a40c5ce93ef5d83012bde2980f75
Daniel Suarez <dansuarmar@gmail.com>
2023-12-31 16:06:36 -0500

    Prop Fowarding
    Props are not fowarded automatically to children, you have to receive them and use them manually
    But you can use Fowarder Props. This is done by using the spread operator when destructuring, and then again in the styles.
    
    We implemented this for the id and class on Examples, and the function that handles the click in TabButton.

--- reactcompleteguide/01-starting-project/src/components/Examples.jsx
+++ reactcompleteguide/01-starting-project/src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { EXAMPLES } from "../data";
 import TabButton from "./TabButton";
+import Section from "./Section";
 
 export default function Examples(){
     const [selectedTopic, setSelectedTopic] = useState();
@@ -25,27 +26,26 @@ export default function Examples(){
     }
 
     return (
-        <section id="examples">
-        <h2>Examples</h2>
-        <menu>
-          <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
-          <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-          <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
-          <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
-        </menu>
-        {!selectedTopic && <p>Pleade Select a Topic</p>}
-        {!selectedTopic ? <p>Please Select a Topic</p> :
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
-        }
-        {tabContent}
-      </section>
+        <Section id="examples" title="Examples">
+            <menu>
+            <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleSelect('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleSelect('state')}>State</TabButton>
+            </menu>
+            {!selectedTopic && <p>Pleade Select a Topic</p>}
+            {!selectedTopic ? <p>Please Select a Topic</p> :
+            <div id="tab-content">
+                <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                <code>
+                    {EXAMPLES[selectedTopic].code}
+                </code>
+                </pre>
+            </div>
+            }
+            {tabContent}
+        </Section>
     );
 }
